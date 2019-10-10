f783add9d4255e628d9368655384aa9cfecbfd4a
768661 <768661@ELF8IT221608.ACDM.DS.SAIT.CA>
2019-10-10 17:27:21 -0600

    Get Users The methods: getUser() and getAll() have been created.

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -1,5 +1,8 @@
 package dataaccess;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import models.User;
 import java.util.List;
 
@@ -13,12 +16,46 @@ public class UserDB {
         return 0;
     }
 
+    /**
+     * This method queries the database for all users. Every user (dude) is put into an ArrayList of users (dudes).
+     * @return ArrayList dudes
+     * @throws NotesDBException 
+     */
     public List<User> getAll() throws NotesDBException {
-        return null;
+        User dude;
+        ArrayList<User> dudes = new ArrayList<>();
+        
+        String preparedSQL = "SELECT active, email, fname, lname FROM user_table";
+        PreparedStatement ps = connection.prepareStatement(preparedSQL);
+        ResultSet product = ps.executeQuery();
+        
+        while(product.next()){
+            User dude = new User(product.getBoolean(), product.getString(),
+                                product.getString(), product.getString());
+            dudes.add(dude);
+        }
+
+        return dudes;
     }
 
-    public User getUser(String username) throws NotesDBException {
-        return null;
+    /**
+     * This method queries the database for a particular user (dude) that has a matching email.
+     * @param email
+     * @return User dude
+     * @throws NotesDBException 
+     */
+    public User getUser(String email) throws NotesDBException {
+        String preparedSQL = "SELECT active, email, fname, lname FROM user_table WHERE email = ?";
+        PreparedStatement ps = connection.prepareStatement(preparedSQL);
+        ps.setString(1, email);
+        ResultSet product = ps.executeQuery();
+        
+        while(product.next()){
+            User dude = new User(product.getBoolean(), product.getString(),
+                                product.getString(), product.getString());
+        }
+
+        return dude;
     }
 
     public int delete(User user) throws NotesDBException {
