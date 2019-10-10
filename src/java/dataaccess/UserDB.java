383283a7df2bd535d16e9f7add4bb834f3293283
652343 <652343@ELF8IT221633.ACDM.DS.SAIT.CA>
2019-10-10 17:33:54 -0600

    Initialize update method

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -1,16 +1,34 @@
 package dataaccess;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import models.User;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserDB {
+    private Connection connection;
+    
+    private static final String UPDATE_STATEMENT = "UPDATE User_Table set fname=? lname=? password=? where active = true and email=?";
 
     public int insert(User user) throws NotesDBException {
-        return 0;
+        
     }
 
     public int update(User user) throws NotesDBException {
-        return 0;
+        
+        try
+          {
+            PreparedStatement statement = connection.prepareStatement(UPDATE_STATEMENT);
+            statement.setString(1, c.getFirstName());
+            
+            
+          } catch (SQLException ex)
+          {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, ex);
+          }
     }
 
     public List<User> getAll() throws NotesDBException {
