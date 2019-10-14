8bdeeb92b6a2e34e4f419efe5e2c8386444e7b83
yaehjs <arranwoodruff@gmail.com>
2019-10-14 11:07:13 -0600

    WIP

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -5,23 +5,43 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.User;
 
 public class UserServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String firstname = request.getParameter("firstname");
-        String lastname = request.getParameter("lastname");
-        String password = request.getParameter("password");
-        String email = request.getParameter("email");
-        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    String fname = request.getParameter("fname");
+    String lname = request.getParameter("lname");
+    String email = request.getParameter("email");
+    String password = request.getParameter("password");
+
+    String action = request.getParameter("action");
+    action = action == null ? "" : action;
+
+    switch (action) {
+      case "edit": {
+        if (fname == null || fname.equals("")
+                || lname == null || lname.equals("")
+                || email == null || email.equals("")) {
+          response.sendRedirect("users");
+          return;
+        }
+        User userToEdit = new User(email, fname, lname, "password");
+        request.setAttribute("edit", true);
+        request.setAttribute("user", userToEdit);
+        break;
+      }
+
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+  }
 
-    }
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+
+  }
 }
