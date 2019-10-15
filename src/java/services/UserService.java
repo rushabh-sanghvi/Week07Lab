5f4f5b519bd5af6b543233bf641a09f52620fddf
768661 <768661@ELF8IT221608.ACDM.DS.SAIT.CA>
2019-10-15 09:25:55 -0600

    Get Methods The methods get() and getAll() have been made.

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -2,26 +2,43 @@ package services;
 
 import models.User;
 import java.util.List;
+import dataaccess.UserDB;
+import java.util.ArrayList;
 
 public class UserService {
 
-    public User get(String username) throws Exception {
-        return null;
+    /**
+     * This method calls the getUser() method in UserDB.
+     * @param email - the user's email to identify them in the database.
+     * @return user - the user that is requested.
+     * @throws Exception - all exceptions that could be had.
+     */
+    public User get(String email) throws Exception {
+        User user = db.getUser(email);
+        
+        return user;
     }
 
+    /**
+     * This method calls the getAll() method from UserDB.
+     * @return userList - a list of users from the database.
+     * @throws Exception - all exceptions that could be had.
+     */
     public List<User> getAll() throws Exception {
-        return null;
+        ArrayList<User> userList = (ArrayList<User>) db.getAll();
+        
+        return userList;
     }
 
-    public int update(String username, String password) throws Exception {
+    public int update(String email, String password) throws Exception {
         return 0;
     }
 
-    public int delete(String username) throws Exception {
+    public int delete(String email) throws Exception {
         return 0;
     }
 
-    public int insert(String username, String password) throws Exception {
+    public int insert(String email, String password) throws Exception {
         return 0;
     }
 
