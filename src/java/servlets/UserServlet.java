e8da0626fcb62943f4a2793e4e84120d12dabe5a
yaehjs <arranwoodruff@gmail.com>
2019-10-15 09:31:15 -0600

    Started doPost and doGet

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -1,18 +1,20 @@
 package servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import models.User;
+import services.UserService;
 
 public class UserServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
           throws ServletException, IOException {
+    UserService us = new UserService();
+
     String fname = request.getParameter("fname");
     String lname = request.getParameter("lname");
     String email = request.getParameter("email");
@@ -27,27 +29,36 @@ public class UserServlet extends HttpServlet {
         request.setAttribute("user", null);
         break;
       case "edit": {
-        System.out.println(email + lname + fname);
-        if (fname == null || fname.equals("")
-                || lname == null || lname.equals("")
-                || email == null || email.equals("")) {
+        String[] values = new String[3];
+        values[0] = fname;
+        values[1] = lname;
+        values[2] = email;
+        if (!checkFalsey(values)) {
           response.sendRedirect("users");
           return;
         }
-        User userToEdit = new User(email, fname, lname, "password");
         request.setAttribute("edit", true);
-        request.setAttribute("user", userToEdit);
+        try {
+          request.setAttribute("user", us.get(email));
+        } catch (Exception e) {
+          request.setAttribute("error", "Could not retrieve user.");
+        }
         break;
       }
-
+      case "delete":
+        try {
+          us.delete(email);
+        } catch (Exception e) {
+          request.setAttribute("error", "Could not delete user.");
+        }
+        break;
     }
 
-    ArrayList<User> users = new ArrayList<>();
-    User testUser = new User("testEmail", "testFname", "testLname", "password");
-    users.add(testUser);
-    users.add(testUser);
-    users.add(testUser);
-    request.setAttribute("users", users);
+    try {
+      request.setAttribute("users", us.getAll());
+    } catch (Exception e) {
+      request.setAttribute("error", "Could not retrive users.");
+    }
 
     getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
   }
@@ -55,6 +66,8 @@ public class UserServlet extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
           throws ServletException, IOException {
+    UserService us = new UserService();
+
     String fname = request.getParameter("fname");
     String lname = request.getParameter("lname");
     String email = request.getParameter("email");
@@ -62,9 +75,32 @@ public class UserServlet extends HttpServlet {
 
     String action = request.getParameter("action");
     action = action == null ? "" : action;
-    
-    switch (action){
-      // todo action cases
+
+    String[] values = new String[3];
+    values[0] = fname;
+    values[1] = lname;
+    values[2] = email;
+
+    if (!checkFalsey(values)) {
+      response.sendRedirect("users");
+      return;
     }
+
+    switch (action) {
+      case "add": {
+        
+      }
+    }
+  }
+
+  private boolean checkFalsey(String[] values) {
+    // check each elemenet in array for null or empty string
+    // return false if one is found
+    for (String s : values) {
+      if (s == null || s.equals("")) {
+        return false;
+      }
+    }
+    return true;
   }
 }
