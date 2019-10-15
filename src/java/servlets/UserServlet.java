d8ced5f78dc78a40fb0e79190d4af31cabfae865
745507 <745507@SAIT221315.ACDM.DS.SAIT.CA>
2019-10-15 11:31:16 -0600

    Added saving users Fixed UserServlet to save users

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.User;
 import services.UserService;
 
 public class UserServlet extends HttpServlet {
